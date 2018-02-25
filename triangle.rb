3d33dc88af7237f618e46681b676927f771636ad
Fishercoder <fishercoder@gmail.com>
2018-02-25 09:07:12 -0800

    finished triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  # this is messy code I wrote on 2/25/2018, what's the more Ruby style to write this?
+  if a == b and b == c
+    return :equilateral
+  else if a == b or b == c or a == c
+         return :isosceles
+       end
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
