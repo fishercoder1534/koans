08c8893ad6b30917d08c2bebb237770066c9247f
Steve Sun <fishercoder@gmail.com>
2018-02-26 08:46:04 -0800

    finished about_scoring_project.rb

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,30 +30,48 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
-  return 0 unless dice.any?
-  dice = [dice].sort
-  points = 0
-  if dice[0] == dice[2]
-    if dice[0] == 1
-      points = 1000
-      if dice[0] == 2
-        points = 200
-      end
-      if dice[0] == 3
-        points = 300
-      end
-      if dice[0] == 4
-        points = 400
-      end
-      if dice[0] == 5
-        points = 500
-      end
-      if dice[0] == 6
-        points = 600
-      end
+  # reference: https://github.com/javierjulio/ruby-koans-completed/blob/master/about_scoring_project.rb
+  result = 0
+
+  # First idea: loop through all values and add to hash but only
+  # once per unique value and have a counter for how many times
+  # value was found
+
+  # The param for the new() method call acts as a default value if
+  # an accessed key doesn't exist. Awesome! Saved me from writing a
+  # bloated each statement with an if/elsif to handle existence!
+  counts = Hash.new(0)
+
+  dice.each do |value|
+    counts[value] += 1
+  end
+
+  counts.each do |item,numFound|
+    # 1,1,1 = 1000 points
+    if item == 1 && numFound >= 3 then
+      result += 1000
+      numFound -= 3
+    end
+
+    # any number other than 1, found 3 times is that number times
+    # 100.. so 5,5,5 = 500 points, 3,3,3 = 300 points, etc.
+    if item != 1 && numFound >= 3 then
+      result += item * 100
+      numFound -= 3
+    end
+
+    # 1 (not part of set) = 100 points for each found
+    if item == 1 && numFound <= 2 then
+      result += 100 * numFound
+    end
+
+    # 5 (not part of set) = 50 points for each found
+    if item == 5 && numFound <=2 then
+      result += 50 * numFound
     end
   end
+
+  result
 end
 
 class AboutScoringProject < Neo::Koan
@@ -61,40 +79,40 @@ class AboutScoringProject < Neo::Koan
     assert_equal 0, score([])
   end
 
-  # def test_score_of_a_single_roll_of_5_is_50
-  #   assert_equal 50, score([5])
-  # end
-  #
-  # def test_score_of_a_single_roll_of_1_is_100
-  #   assert_equal 100, score([1])
-  # end
-  #
-  # def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-  #   assert_equal 300, score([1,5,5,1])
-  # end
-  #
-  # def test_score_of_single_2s_3s_4s_and_6s_are_zero
-  #   assert_equal 0, score([2,3,4,6])
-  # end
-  #
-  # def test_score_of_a_triple_1_is_1000
-  #   assert_equal 1000, score([1,1,1])
-  # end
-  #
-  # def test_score_of_other_triples_is_100x
-  #   assert_equal 200, score([2,2,2])
-  #   assert_equal 300, score([3,3,3])
-  #   assert_equal 400, score([4,4,4])
-  #   assert_equal 500, score([5,5,5])
-  #   assert_equal 600, score([6,6,6])
-  # end
-  #
-  # def test_score_of_mixed_is_sum
-  #   assert_equal 250, score([2,5,2,2,3])
-  #   assert_equal 550, score([5,5,5,5])
-  #   assert_equal 1100, score([1,1,1,1])
-  #   assert_equal 1200, score([1,1,1,1,1])
-  #   assert_equal 1150, score([1,1,1,5,1])
-  # end
+  def test_score_of_a_single_roll_of_5_is_50
+    assert_equal 50, score([5])
+  end
+
+  def test_score_of_a_single_roll_of_1_is_100
+    assert_equal 100, score([1])
+  end
+
+  def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
+    assert_equal 300, score([1,5,5,1])
+  end
+
+  def test_score_of_single_2s_3s_4s_and_6s_are_zero
+    assert_equal 0, score([2,3,4,6])
+  end
+
+  def test_score_of_a_triple_1_is_1000
+    assert_equal 1000, score([1,1,1])
+  end
+
+  def test_score_of_other_triples_is_100x
+    assert_equal 200, score([2,2,2])
+    assert_equal 300, score([3,3,3])
+    assert_equal 400, score([4,4,4])
+    assert_equal 500, score([5,5,5])
+    assert_equal 600, score([6,6,6])
+  end
+
+  def test_score_of_mixed_is_sum
+    assert_equal 250, score([2,5,2,2,3])
+    assert_equal 550, score([5,5,5,5])
+    assert_equal 1100, score([1,1,1,1])
+    assert_equal 1200, score([1,1,1,1,1])
+    assert_equal 1150, score([1,1,1,5,1])
+  end
 
 end
